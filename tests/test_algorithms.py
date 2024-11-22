acdee37fae73da3b11422966c5f5e40bcace4f84
Miguel Muñoz <emwyes158@gmail.com>
2024-11-22 12:48:52 -0600

    test_algorithms.py

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,6 +1,6 @@
 import unittest
 from graph import Graph
-from utils.errors import NodeNotFoundError
+from utils.errors import NodeNotFoundError, EdgeNotFoundError
 
 class TestGraph(unittest.TestCase):
 
