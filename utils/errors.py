c9c9ab3063c2402d7a79879af4fab203f511e823
Miguel Muñoz <emwyes158@gmail.com>
2024-11-22 12:54:11 -0600

    errors.py

--- utils/errors.py
+++ utils/errors.py
@@ -1,11 +1,11 @@
 class NodeNotFoundError(Exception):
-    # Exception raised when a node is not found in the graph.
+    # Exception raised when a node is not found in a graph
     def __init__(self, node):
         super().__init__(f"Node '{node}' not found in the graph.")
         self.node = node
 
 class EdgeNotFoundError(Exception):
-    # Exception raised when an edge is not found in the graph.
+    # Exception raised when an edge is not found in a graph
     def __init__(self, origin, destination):
         super().__init__(f"Edge from '{origin}' to '{destination}' not found in the graph.")
         self.origin = origin
